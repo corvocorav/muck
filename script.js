05ea4bc7a61bded03699337201bfcb33cfa3bcb0
corvocorav <corvojaeger@gmail.com>
2021-07-29 08:56:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+setTimeout(() => {
+    alert("this is a fan website not an official one, have fun!");
+}, 200);
+
+function discord() 
+{
+    window.open("https://discord.gg/ZRwQYCTd");
+}
+function speedrun() 
+{
+    window.open("https://www.speedrun.com/muck");
+}
+function youtube() 
+{
+    window.open("https://www.youtube.com/channel/UCIabPXjvT5BVTxRDPCBBOOQ");
+}
+function steam() 
+{
+    window.open("https://store.steampowered.com/app/1625450/Muck/");
+}
+
+function mydiscord() 
+{
+    alert("coroxovooc#0011")
+}
+function github() 
+{
+    window.open("https://github.com/corvocorav");
+}
